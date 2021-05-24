cff31ce878dcba0f73e216a11524fbcd344c7bb6
Valentin CLENET <valentin.clenet@witty-services.fr>
2021-05-24 15:18:19 +0200

    fix: fix take-until-destroy test

--- projects/ngx-common/src/lib/take-until-destroy/take-until-destroy.operator.spec.ts
+++ projects/ngx-common/src/lib/take-until-destroy/take-until-destroy.operator.spec.ts
@@ -3,10 +3,12 @@ import 'reflect-metadata';
 import { interval, Subscription } from 'rxjs';
 import { takeUntilDestroy } from './take-until-destroy.operator';
 import { OnDestroy } from '@angular/core';
+import {OnDestroyListener} from './on-destroy-listener.decorator';
 
 describe('takeUntilDestroy', () => {
 
   it('should unsubscribe on component destroy', () => {
+    @OnDestroyListener()
     class A {
 
       public subscription: Subscription;
@@ -24,6 +26,7 @@ describe('takeUntilDestroy', () => {
   });
 
   it('should unsubscribe on component destroy with custom destroy method', () => {
+    @OnDestroyListener()
     class A implements OnDestroy {
 
       public subscription: Subscription;
