8a56f745f28aa0bbd8c1b8a2a2f56d505cad220d
Valentin CLENET <valentin.clenet@witty-services.fr>
2021-05-24 14:53:40 +0200

    fix: fix lint issues

--- projects/ngx-common/src/lib/on-attribute-change/on-attribute-change.decorator.spec.ts
+++ projects/ngx-common/src/lib/on-attribute-change/on-attribute-change.decorator.spec.ts
@@ -7,38 +7,42 @@ describe('OnAttributeChangeDecorator', () => {
   it('should send value to implicit property', (done: DoneFn) => {
     class Test {
       public attr: string;
-      @OnAttributeChange() attr$: Observable<string>;
+
+      @OnAttributeChange()
+      public attr$: Observable<string>;
     }
 
-    const test = new Test();
+    const test: Test = new Test();
 
     test.attr$.pipe(
       ifNotNull()
     ).subscribe(
-      (value) => {
-        expect(value).toBe("Test 1");
+      (value: string) => {
+        expect(value).toBe('Test 1');
         done();
       }
     );
-    test.attr = "Test 1";
+    test.attr = 'Test 1';
   });
 
   it('should send value to specified property', (done: DoneFn) => {
     class Test2 {
       public attr: string;
-      @OnAttributeChange('attr') attrChange$: Observable<string>;
+
+      @OnAttributeChange('attr')
+      public attrChange$: Observable<string>;
     }
 
-    const test2 = new Test2();
+    const test2: Test2 = new Test2();
 
     test2.attrChange$.pipe(
       ifNotNull()
     ).subscribe(
-      (value) => {
-        expect(value).toBe("Test 2");
+      (value: string) => {
+        expect(value).toBe('Test 2');
         done();
       }
     );
-    test2.attr = "Test 2";
+    test2.attr = 'Test 2';
   });
 });
